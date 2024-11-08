42d5eacaea88d50fef6d79d32e4378676a2eba4d
AR <52975920+AR13ar@users.noreply.github.com>
2024-11-08 17:29:15 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,8 +8,8 @@ let gameOver = false;
 // Box object
 class Box {
     constructor(url, imageUrl, text) {
-        this.x = Math.random() * (canvas.width - 160);
-        this.y = Math.random() * (canvas.height - 160);
+        this.x = 400 //Math.random() * (canvas.width - 160);
+        this.y = 250//Math.random() * (canvas.height - 160);
         this.size = 80;
         this.dx = 1 + Math.random() * 2; // Horizontal speed
         this.dy = 1 + Math.random() * 2; // Vertical speed
